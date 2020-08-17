e530935918256c7d91d3725541fd9cc1810477d2
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-08-17 19:39:46 -0400

    working app

--- Develop/app.js
+++ Develop/app.js
@@ -7,21 +7,51 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const outputPath = path.join(OUTPUT_DIR, "main.html");
 
 const render = require("./lib/htmlRenderer");
-
+const team = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
+function addMore() {
+    inquirer.prompt([{
+        type: "list",
+        choices: ["Yes", "No"],
+        message:"Would you like to add more team members?",
+        name: "addmore",
+    }]).then(function(response){
+        this.response = response.addmore;
+        if (this.response === "Yes") {
+            return askQuestions();
+        } else {
+            if (!fs.existsSync(OUTPUT_DIR)) {
+                fs.mkdirSync(OUTPUT_DIR);
+            } 
+            fs.writeFile(outputPath, render(team), "utf8", function(error) {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                console.log("Success!");
+            });
+            return console.log("HTML has succesfully been created!");
+        }
+    });
+}
+
+
+function askQuestions() {
 
 inquirer.prompt([
    {
        type: "list",
        choices: ["Manager", "Engineer", "Intern"],
        message:"What is your role in the company?",
-       name: "position",
+       name: "role",
    },
    {
        type: "input",
@@ -39,27 +69,37 @@ inquirer.prompt([
        name: "email",
    },
 ]).then(function(response){
-    this.position = response.position;
     this.name = response.name;
     this.id = response.id;
     this.email = response.email;
-
+    this.role = response.role;
+    
     // test code
-    const emailSymbol = "@";
-    const comSymbol = ".com";
-    function checkEmail() {
-        let email = this.email.split("");
+    // const emailSymbol = "@";
+    // const comSymbol = ".com";
+    // function checkEmail() {
+    //     let email = this.email.split("");
 
-            if (email.includes(emailSymbol) && email.includes(comSymbol)) {
-                return;
-            } else {
-                return console.log("Please enter a valid email address.");
-            }
-    }
-    checkEmail();
+    //         if (email.includes(emailSymbol) && email.includes(comSymbol)) {
+    //             return;
+    //         } else {
+    //             console.log("Please enter a valid email address.");
+    //             inquirer.prompt([
+                    
+    //                 {
+    //                     type: "email",
+    //                     message:"What is your email address?",
+    //                     name: "email",
+    //                 }
+    //              ]).then(function(response){
+    //                  this.email = response.email; 
+    //              });
+    //         }
+    // }
+    // checkEmail();
     //test code
 
-    if (this.position === "Manager") {
+    if (this.role === "Manager") {
         inquirer.prompt([
             {
                 type: "input",
@@ -67,15 +107,14 @@ inquirer.prompt([
                 name: "officeNumber",
             }
         ]).then(function(response) {
-            fs.appendFile("main.html", new Manager(response), function(error){
-                if (error) {
-                    console.log(error);
-                    return;
-                }
+                this.officeNumber = response.officeNumber;
+                const newManager = new Manager(this.name, this.id, this.email, this.officeNumber);
                 console.log("Success! The team has a manager.");
-            }) 
-        })
-    } else if (this.position === "Engineer") {
+                team.push(newManager);
+            }).then(function() {
+                addMore();
+            });
+    } else if (this.role === "Engineer") {
         inquirer.prompt([
             {
                 type: "input",
@@ -83,36 +122,34 @@ inquirer.prompt([
                 name: "GitHub",
             }
         ]).then(function(response) {
-            fs.appendFile("engineer.html", new Engineer(response), function(error){
-                if (error) {
-                    console.log(error);
-                    return;
-                }
+                this.Github = response.Github;
+                const newEngineer = new Engineer(this.name, this.id, this.email, this.Github);
                 console.log("Success! Added an Engineer to the team.");
-            }) 
-        })
-    } else if (this.position === "intern") {
+                team.push(newEngineer);
+            }).then(function() {
+                addMore();
+            });
+    } else if (this.role === "Intern") {
         inquirer.prompt([
             {
                 type: "input",
-                message: "What is your GitHub account name?",
-                name: "GitHub",
+                message: "What college did you go to?",
+                name: "school",
             }
         ]).then(function(response) {
-            fs.appendFile("intern.html", Intern(response), function(error){
-                if (error) {
-                    console.log(error);
-                    return;
-                }
-                console.log("Success! Added an intern to the team.");
+            this.school = response.school;
+            const newIntern = new Intern(this.name, this.id, this.email, this.school);
+            console.log("Success! Added an intern to the team.");
+            team.push(newIntern);
+            }).then(function() {
+                addMore();
             }); 
-        }); 
+        }
+        
     }
-});
+)}
 
-
-const newEmployee = new Employee("this.name", 4, "name@name.com");
-
+askQuestions();
 
 
 // After the user has input all employees desired, call the `render` function (required
