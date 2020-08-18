cb02bf9204e578ae8ae381ae911a8a942ad1ead2
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-08-17 22:30:56 -0400

    working build

--- Develop/app.js
+++ Develop/app.js
@@ -5,17 +5,13 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+var validator = require("email-validator");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "main.html");
 
 const render = require("./lib/htmlRenderer");
 const team = [];
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 
 function addMore() {
     inquirer.prompt([{
@@ -36,13 +32,11 @@ function addMore() {
                     console.log(error);
                     return;
                 }
-                console.log("Success!");
             });
             return console.log("HTML has succesfully been created!");
         }
     });
 }
-
 
 function askQuestions() {
 
@@ -63,111 +57,76 @@ inquirer.prompt([
        message:"What is your ID number?",
        name: "id",
    },
-   {
-       type: "email",
-       message:"What is your email address?",
-       name: "email",
-   },
 ]).then(function(response){
     this.name = response.name;
     this.id = response.id;
-    this.email = response.email;
-    this.role = response.role;
-    
-    // test code
-    // const emailSymbol = "@";
-    // const comSymbol = ".com";
-    // function checkEmail() {
-    //     let email = this.email.split("");
+    this.role = response.role;  
 
-    //         if (email.includes(emailSymbol) && email.includes(comSymbol)) {
-    //             return;
-    //         } else {
-    //             console.log("Please enter a valid email address.");
-    //             inquirer.prompt([
+    function checkEmail() {
+        inquirer.prompt([
+            {
+                type: "email",
+                message:"What is your email address?",
+                name: "email",
+            }
+         ]).then(function(response){
+            this.email = response.email;
+            if (validator.validate(this.email)) {
+                this.email = response.email;
+                if (this.role === "Manager") {
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            message: "What is your office number?",
+                            name: "officeNumber",
+                        }
+                    ]).then(function(response) {
+                            this.officeNumber = response.officeNumber;
+                            const newManager = new Manager(this.name, this.id, this.email, this.officeNumber);
+                            console.log("Success! The team has a manager.");
+                            team.push(newManager);
+                        }).then(function() {
+                            addMore();
+                        });
+                } else if (this.role === "Engineer") {
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            message: "What is your GitHub account name?",
+                            name: "GitHub",
+                        }
+                    ]).then(function(response) {
+                            this.Github = response.Github;
+                            const newEngineer = new Engineer(this.name, this.id, this.email, this.Github);
+                            console.log("Success! Added an Engineer to the team.");
+                            team.push(newEngineer);
+                        }).then(function() {
+                            addMore();
+                        });
+                } else if (this.role === "Intern") {
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            message: "What college did you go to?",
+                            name: "school",
+                        }
+                    ]).then(function(response) {
+                        this.school = response.school;
+                        const newIntern = new Intern(this.name, this.id, this.email, this.school);
+                        console.log("Success! Added an intern to the team.");
+                        team.push(newIntern);
+                        }).then(function() {
+                            addMore();
+                        });
                     
-    //                 {
-    //                     type: "email",
-    //                     message:"What is your email address?",
-    //                     name: "email",
-    //                 }
-    //              ]).then(function(response){
-    //                  this.email = response.email; 
-    //              });
-    //         }
-    // }
-    // checkEmail();
-    //test code
-
-    if (this.role === "Manager") {
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is your office number?",
-                name: "officeNumber",
+                    };
+            } else {
+                console.log("Please enter a valid email address.");
+                checkEmail();
             }
-        ]).then(function(response) {
-                this.officeNumber = response.officeNumber;
-                const newManager = new Manager(this.name, this.id, this.email, this.officeNumber);
-                console.log("Success! The team has a manager.");
-                team.push(newManager);
-            }).then(function() {
-                addMore();
-            });
-    } else if (this.role === "Engineer") {
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is your GitHub account name?",
-                name: "GitHub",
-            }
-        ]).then(function(response) {
-                this.Github = response.Github;
-                const newEngineer = new Engineer(this.name, this.id, this.email, this.Github);
-                console.log("Success! Added an Engineer to the team.");
-                team.push(newEngineer);
-            }).then(function() {
-                addMore();
-            });
-    } else if (this.role === "Intern") {
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What college did you go to?",
-                name: "school",
-            }
-        ]).then(function(response) {
-            this.school = response.school;
-            const newIntern = new Intern(this.name, this.id, this.email, this.school);
-            console.log("Success! Added an intern to the team.");
-            team.push(newIntern);
-            }).then(function() {
-                addMore();
-            }); 
-        }
-        
-    }
-)}
-
+        })
+    };
+    checkEmail();
+    });
+};
 askQuestions();
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!
